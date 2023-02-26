12b0cb965a67937765db9900692f77424da1820c
seniordotnet <alexander.titarenko4@gmail.com>
2023-02-26 15:33:26 +0300

    Complete model

---
+++ PSMapper/Poco/TraceRt/TraceRtInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PSMapper.Poco.TraceRt;
+
+public class TraceRtInfo
+{
+    [JsonPropertyName("domainName")] public string? DomainName { get; set; }
+
+    [JsonPropertyName("ipAddress")] public string? IpAddress { get; set; }
+
+    [JsonPropertyName("data")] public List<TraceRtInfoData> Data { get; set; } = new();
+
+    public class TraceRtInfoData
+    {
+        [JsonPropertyName("firstPacket")] public int FirstPacket { get; set; }
+
+        [JsonPropertyName("secondPacket")] public int SecondPacket { get; set; }
+
+        [JsonPropertyName("thirdPacket")] public int ThirdPacket { get; set; }
+
+        [JsonPropertyName("ipAddress")] public string? IpAddress { get; set; }
+    }
+}
