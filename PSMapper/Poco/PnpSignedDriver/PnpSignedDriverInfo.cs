bf9d424c15a06439be5c1bf123e728201e3f31d3
seniordotnet <alexander.titarenko4@gmail.com>
2023-02-27 13:28:04 +0300

    Add PnpSignedDriver command

---
+++ PSMapper/Poco/PnpSignedDriver/PnpSignedDriverInfo.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PSMapper.Poco.PnpSignedDriver;
+
+public class PnpSignedDriverInfo
+{
+    public List<PnpSignedDriverInfoData> Data { get; set; } = new();
+
+    public class PnpSignedDriverInfoData
+    {
+        [JsonPropertyName("runspaceId")] public Guid? RunspaceId { get; set; }
+
+        [JsonPropertyName("caption")] public string? Caption { get; set; }
+
+        [JsonPropertyName("classGuid")] public Guid? ClassGuid { get; set; }
+
+        [JsonPropertyName("compatId")] public string? CompatId { get; set; }
+
+        [JsonPropertyName("creationClassName")]
+        public string? CreationClassName { get; set; }
+
+        [JsonPropertyName("description")] public string? Description { get; set; }
+
+        [JsonPropertyName("deviceClass")] public string? DeviceClass { get; set; }
+
+        [JsonPropertyName("deviceId")] public string? DeviceId { get; set; }
+
+        [JsonPropertyName("driverProviderName")]
+        public string? DriverProviderName { get; set; }
+
+        [JsonPropertyName("driverVersion")] public string? DriverVersion { get; set; }
+
+        [JsonPropertyName("friendlyName")] public string? FriendlyName { get; set; }
+
+        [JsonPropertyName("hardwareId")] public string? HardwareId { get; set; }
+
+        [JsonPropertyName("infName")] public string? InfName { get; set; }
+
+        [JsonPropertyName("isSigned")] public bool? IsSigned { get; set; }
+
+        [JsonPropertyName("manufacturer")] public string? Manufacturer { get; set; }
+
+        [JsonPropertyName("pdo")] public string? Pdo { get; set; }
+
+        [JsonPropertyName("name")] public string? Name { get; set; }
+    }
+}
