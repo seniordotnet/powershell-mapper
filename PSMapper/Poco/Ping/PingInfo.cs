06f10dd08341a3b985c72bc276577080ccc88ea4
seniordotnet <alexander.titarenko4@gmail.com>
2023-02-27 13:42:34 +0300

    Add ping poco class

---
+++ PSMapper/Poco/Ping/PingInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PSMapper.Poco.Ping;
+
+public class PingInfo
+{
+    [JsonPropertyName("domain")]
+    public string? Domain { get; set; }
+
+    [JsonPropertyName("ipAddress")]
+    public string? IpAddress { get; set; }
+
+    public class PingInfoData
+    {
+        [JsonPropertyName("ttl")]
+        public int Ttl { get; set; }
+        
+        [JsonPropertyName("time")]
+        public int Time { get; set; }
+        
+        [JsonPropertyName("bufferSize")]
+        public int BufferSize { get; set; }
+    }
+}
